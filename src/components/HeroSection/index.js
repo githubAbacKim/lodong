c2f9205cd1b1412605e1789062eefff5ff0f6ae9
markvaldez <markbrvaldez@gmail.com>
2022-12-31 12:44:38 +0800

    mark update happy new year

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import {
   HeroContainer,
   HeroBg,
-  ImageBg,
   HeroContent,
   HeroH1,
   HeroP,
   HeroH4,
+  HeroBgOverlay,
 } from './HereElements';
 import bgOpacity from '../../images/bgOpacity.svg';
+import bgpersonImage from '../../images/bgpersonImage.svg';
 
 const HeroSection = () => {
   return (
     <HeroContainer>
       <HeroBg>
-        <ImageBg src={bgOpacity} />
+        <img src={bgOpacity} width="1100px" height="1000px" />
       </HeroBg>
+      <HeroBgOverlay>
+        <img src={bgpersonImage} width="800px" height="1000px" />
+      </HeroBgOverlay>
+
       <HeroContent>
         <HeroH1>실패없는 개발</HeroH1>
         <HeroP>모든 혁신은 불가능 위에 쓰여졌다.</HeroP>
         <HeroP>
           수만년전, 그 어떤 인류도 사냥감을 조리 할 수 있다고 생각하지
-          못했습니다. 모든 가능성을 열어 두었던 한 창의적인 인류가 불을 발견
-          하기 전까지 모든 유럽인들은 대서양을 가로질러 인도를 찾는것은
-          불가능이라고 말했습니다.
+          못했습니다. <br />
+          모든 가능성을 열어 두었던 한 창의적인 인류가 불을 발견 하기 전까지
+          <br />
+          모든 유럽인들은 대서양을 가로질러 인도를 찾는것은 불가능이라고
+          말했습니다.
         </HeroP>
 
         <HeroH4>
