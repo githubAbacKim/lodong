0551219b86b67f7d8386f1fd34b7a80f99752f00
jhon mark valdez <markbrvaldez@gmail.com>
2023-01-03 17:27:07 +0800

    MARK UPATE

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -2,6 +2,56 @@ import styled from "styled-components";
 
 
 
+export const FooterContainer = styled.div`
+
+@media screen and (max-width: 575.98px) {
+  display:blockl
+}
+
+@media  screen and  (min-width: 576px) and (max-width: 767.98px) {
+ 
+}
+
+@media screen and  (min-width: 768px) and (max-width: 991.98px) {
+ 
+}
+
+@media  screen and  (min-width: 992px) and (max-width: 1199.98px) {
+
+}
+
+@media  screen and (min-width: 1200px) and (min-width: 2800px) {
+
+}
+@media  screen and  (min-width: 1200px) and (min-width: 2800px) {
+
+}
+`;
+export const FooterRow = styled.div`
+@media screen and (max-width: 575.98px) {
+  margin-left: 0rem;
+}
+
+@media  screen and  (min-width: 576px) and (max-width: 767.98px) {
+  margin-left: 0rem;
+}
+
+@media screen and  (min-width: 768px) and (max-width: 991.98px) {
+  margin-left: 0rem;
+}
+
+@media  screen and  (min-width: 992px) and (max-width: 1199.98px) {
+
+}
+
+@media  screen and (min-width: 1200px) and (min-width: 2800px) {
+
+}
+@media  screen and  (min-width: 1200px) and (min-width: 2800px) {
+
+}
+`;
+
 export const FooterBottom = styled.div`
     display: flex;
     flex-direction: column;
@@ -25,7 +75,7 @@ padding-left: 0rem;
   margin-top: 3vh;
   color: #A3A3A3;
   @media screen and (max-width:   700px) {
-   font-size: 0.5rem;
+   font-size: 1.5rem;
    text-align: center;
 }
   `;
